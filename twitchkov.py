04efe28d56616c6f8e4a1a9ee56d68b23bb7f826
josh <josht0288@gmail.com>
2017-03-18 02:44:27 -0500

    add channel name to output

--- twitchkov.py
+++ twitchkov.py
@@ -20,6 +20,7 @@ while True:
         markov_chain.take_message(msg)
 
     if markov_chain.iterations() % 5 == 1:
+        print("Channel:", markov_chain.dict_name)
         print("Iterations:", markov_chain.iterations())
         print("Time elapsed:", int(markov_chain.time_elapsed()), "seconds")
         print("Message:", markov_chain.make_message())

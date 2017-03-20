172042a1104f9ac22e2663029938e630fd01acbd
Josh Trahan <josh@barbarianna.local>
2017-03-20 12:59:33 -0500

    fix message corruption bug

--- TwitchChat.py
+++ TwitchChat.py
@@ -2,16 +2,21 @@ import socket
 
 DEFAULT_PORT = 6667
 DEFAULT_SRV_URL = "irc.chat.twitch.tv"
+RECV_SIZE = 1024
 
 class TwitchChat():
     def __init__(self, nick, auth, channel = None, url = "irc.chat.twitch.tv",
                  port = 6667):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        join_server(url, port, nick, auth)
+        self.join_server(url, port, nick, auth)
         
         if channel:
             self.join_channel(channel)
+
+        self.message_buffer = ""
+
+        self.unknown_messages = 0
 
     def join_server(self, server_url, port, nick, auth):
         self.sock.connect(("irc.chat.twitch.tv", 6667))
@@ -22,36 +27,60 @@ class TwitchChat():
         print()
         print(welcome_message)
 
-
     def join_channel(self, chan_name):
         self.sock.sendall(("JOIN #" + chan_name + "\r\n").encode("UTF-8"))
         join_message = self.sock.recv(512).decode("UTF-8")
         print(join_message)
 
-    def format_msg(self, raw_msg):
-        if raw_msg[:4] == "PING":
-            print("Got ping:", raw_msg)
-            self.sock.sendall((raw_msg.replace("PING", "PONG") + "\r\n").encode("UTF-8"))
-            print("Responded with pong:", raw_msg.replace("PING", "PONG"))
-            print()
-            return
-        elif "PRIVMSG" not in raw_msg:
-            print("Other msg recieved:", raw_msg)
-            print()
-            return
-        
-        msg_start = raw_msg.find(':', 1) + 1
-        
-        sender_nick = raw_msg[raw_msg.find(':') + 1 : raw_msg.find('!')]
-        channel = raw_msg[raw_msg.find('#') : msg_start - 2]
-        message = raw_msg[msg_start:]
+    def format_privmsg(self, msg):
+        prefix, sep, rest = msg.partition(' ')
+
+        sender_nick = prefix[1:prefix.find('!')]
+
+        operation, sep, rest = rest.partition(' ')
+
+        channel, sep, message = rest.partition(' ')
+
+        message = message[1:]
+
         return (channel, sender_nick, message)
 
+    def handle_ping(self, msg):
+        print("Got ping: {}")
+        url = msg[msg.find(':') + 1:]
+        self.sock.sendall((msg.replace("PING", "PONG") + "\r\n").encode("UTF-8"))
+    
+    # First, should determine what kind of operation the message is.
+    # Then, should delegate to the proper method.
+    # Right now, the only ones I care about are PRIVMSG and PING.
+    def format_message(self, msg):
+        if "PRIVMSG" in msg:
+            return self.format_privmsg(msg)
+        
+        elif "PING" in msg:
+            self.handle_ping(msg)
+
+        else:
+            self.unknown_messages += 1
+            print("Other message recieved:", msg)
+            print("Unknown messages:", self.unknown_messages)
+            return None
+    
+    # Returns list of tuples in the form (channel, sender_nick, message)
     def get_messages(self):
-        raw_msg = self.sock.recv(2048).decode("UTF-8")
+        raw_msg = self.message_buffer + self.sock.recv(RECV_SIZE).decode("UTF-8")
+        
         msg_list = []
-        for msg in raw_msg.split("\r\n"):
-            if msg:
-                msg_list.append(self.format_msg(msg))
+        
+        msg, sep, rest = raw_msg.partition("\r\n")
+
+        while sep == "\r\n":
+            msg_tup = self.format_message(msg)
+            if msg_tup:
+                msg_list.append(msg_tup)
+
+            msg, sep, rest = rest.partition("\r\n")
+
+        self.message_buffer = msg
 
         return msg_list

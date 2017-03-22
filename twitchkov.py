5e7cc8ce6fadbc527e808dcde5ea4449207c1a00
Josh Trahan <josh@barbarianna.local>
2017-03-22 08:51:42 -0500

    Add command line arguments

--- twitchkov.py
+++ twitchkov.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 import TwitchChat
 import MarkovChain
+import sys, getopt
+
+channel = ""
+dict_name = ""
+
+optlist, args = getopt.getopt(sys.argv[1:], "c:f:")
+for option, arg in optlist:
+    if option == "-c":
+        print("Channel found in command line:", arg)
+        channel = arg
+    elif option == "-f":
+        print("File found in command line:", arg)
+        dict_name = arg
+
+if not channel:
+    channel = input("Channel: ")
+if not dict_name:
+    print("No dict_name found, using channel.")
+    dict_name = channel
 
 username = ""
 auth_token = ""
@@ -9,10 +28,8 @@ with open("creds.txt", 'r') as creds_file:
     username = creds_file.readline()
     auth_token = creds_file.readline()
 
-channel = input("Channel: ")
-
 irc_bot = TwitchChat.TwitchChat(username, auth_token, channel)
-markov_chain = MarkovChain.MarkovChain(channel)
+markov_chain = MarkovChain.MarkovChain(dict_name)
 
 while True:
     msg = irc_bot.get_msg()

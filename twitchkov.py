86fe1fc17fad03468853396bfb3ac73c659b466b
j <l@l>
2017-03-24 01:51:37 -0500

    add license text to source files

--- twitchkov.py
+++ twitchkov.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+
+# Copyright (C) 2011-2017 Joshua Trahan
+#
+# This file is part of twitch-markov.
+#
+# twitch-markov is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# twitch-markov is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with twitch-markov.  If not, see <http://www.gnu.org/licenses/>.
+
 import TwitchChat
 import MarkovChain
 import sys, getopt
